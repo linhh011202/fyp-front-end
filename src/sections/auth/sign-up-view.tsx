eea20eb093d1dbe1847307ec7b0a5ca5ed8f4c97
Linh Nguyen Phuong <LINH012@e.ntu.edu.sg>
2025-10-22 02:22:37 +0900

    first commit

---
+++ src/sections/auth/sign-up-view.tsx
@@ -0,0 +1,144 @@
+import { useState, useCallback } from 'react';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { useRouter } from 'src/routes/hooks';
+import { RouterLink } from 'src/routes/components';
+
+import { Iconify } from 'src/components/iconify';
+
+// ----------------------------------------------------------------------
+
+export function SignUpView() {
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSignUp = useCallback(() => {
+    router.push('/');
+  }, [router]);
+
+  const renderForm = (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'flex-end',
+        flexDirection: 'column',
+      }}
+    >
+      <TextField
+        fullWidth
+        name="name"
+        label="Full name"
+        defaultValue=""
+        sx={{ mb: 3 }}
+        slotProps={{
+          inputLabel: { shrink: true },
+        }}
+      />
+
+      <TextField
+        fullWidth
+        name="email"
+        label="Email address"
+        defaultValue=""
+        sx={{ mb: 3 }}
+        slotProps={{
+          inputLabel: { shrink: true },
+        }}
+      />
+
+      <TextField
+        fullWidth
+        name="password"
+        label="Password"
+        defaultValue=""
+        type={showPassword ? 'text' : 'password'}
+        slotProps={{
+          inputLabel: { shrink: true },
+          input: {
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          },
+        }}
+        sx={{ mb: 3 }}
+      />
+
+      <Button
+        fullWidth
+        size="large"
+        type="submit"
+        color="inherit"
+        variant="contained"
+        onClick={handleSignUp}
+      >
+        Sign up
+      </Button>
+    </Box>
+  );
+
+  return (
+    <>
+      <Box
+        sx={{
+          gap: 1.5,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mb: 5,
+        }}
+      >
+        <Typography variant="h5">Sign up</Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            color: 'text.secondary',
+          }}
+        >
+          Already have an account?
+          <Link component={RouterLink} href="/sign-in" variant="subtitle2" sx={{ ml: 0.5 }}>
+            Sign in
+          </Link>
+        </Typography>
+      </Box>
+      {renderForm}
+      <Divider sx={{ my: 3, '&::before, &::after': { borderTopStyle: 'dashed' } }}>
+        <Typography
+          variant="overline"
+          sx={{ color: 'text.secondary', fontWeight: 'fontWeightMedium' }}
+        >
+          OR
+        </Typography>
+      </Divider>
+      <Box
+        sx={{
+          gap: 1,
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <IconButton color="inherit">
+          <Iconify width={22} icon="socials:google" />
+        </IconButton>
+        <IconButton color="inherit">
+          <Iconify width={22} icon="socials:github" />
+        </IconButton>
+        <IconButton color="inherit">
+          <Iconify width={22} icon="socials:twitter" />
+        </IconButton>
+      </Box>
+    </>
+  );
+}
